c532f35a878fc5d5edec43a0f7712c0e35ee6e12
smooti <kobeowens43099@gmail.com>
2022-07-16 15:49:06 -0400

    Moving code to module

--- Invoke-ADEnvironmentLab.ps1
+++ Invoke-ADEnvironmentLab.ps1
@@ -1,27 +1,17 @@
+param (
+	[Parameter (Mandatory = $False, HelpMessage = "ComputerName or IP address.")]
+	[string]$computername = $env:ComputerName,
+
+	[Parameter(Mandatory = $True)]
+	[securestring]$password
+)
+
 # * Confirm we have an elevated session.
 If (-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
 	Throw "You must run this from an elevated PowerShell session."
 }
 
-Write-Verbose "Installing `"AD-Domain-Services`""
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-[securestring]$Password = Read-Host -Prompt 'Enter SafeMode Admin Password' -AsSecureString
+Import-Module -Name .\modules\workflow.psm1
 
-# AD Forrest parameters
-$Params = @{
-	CreateDnsDelegation           = $false
-	DatabasePath                  = 'C:\Windows\NTDS'
-	DomainMode                    = 'WinThreshold'
-	DomainName                    = 'testbed.local'
-	ForestMode                    = 'WinThreshold'
-	InstallDns                    = $true
-	LogPath                       = 'C:\Windows\NTDS'
-	NoRebootOnCompletion          = $true
-	SafeModeAdministratorPassword = $Password
-	SysvolPath                    = 'C:\Windows\SYSVOL'
-	Force                         = $true
-}
-
-# Create Forrest
-Install-ADDSForest @Params
-
+# Setup AD forrest
+setupADForrest -computername $computername -password $password
