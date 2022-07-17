1cd21a247ad1c79023ca490cbbfd23565c3d426a
smooti <kobeowens43099@gmail.com>
2022-07-17 16:48:11 -0400

    Fixing credentials

--- Invoke-ADEnvironment.ps1
+++ Invoke-ADEnvironment.ps1
@@ -26,13 +26,13 @@ if ($CreateTestUsers) {
 	$password = $creds.GetNetworkCredential().Password
 	Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 	$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
-	$credentialsValid = $DS.ValidateCredentials("smooti", $creds.GetNetworkCredential().Password)
+	$credentialsValid = $DS.ValidateCredentials($creds.UserName, $creds.GetNetworkCredential().Password)
 	if ($credentialsValid -ne "True") {
 		throw ("The local credentials that you have provided are invalid.")
 	}
 
 	# Create active directory credentials
-	$adCreds = New-Object System.Management.Automation.PSCredential ("$($DomainName)\$env:UserName", $password)
+	$adCreds = New-Object System.Management.Automation.PSCredential ("$($DomainName)\$($creds.UserName)", $password)
 
 	# Export reusable items to temporary file
 	$adCreds | Export-Clixml -Path "C:\tempCred.xml" -Force
