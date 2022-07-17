8a44d4d3e7d83b56252c79eb69ed5115045c5927
smooti <kobeowens43099@gmail.com>
2022-07-17 16:08:54 -0400

    cleanup temp file

--- New-ADTestUsers.ps1
+++ New-ADTestUsers.ps1
@@ -4,3 +4,6 @@ $testUsers = @("Robb Stark", "Theon Greyjoy", "Ned Stark", "Aria Stark")
 foreach ($user in $testUsers) {
 	New-ADUser -Name $user -Enabled $False -Credential $adCreds
 }
+
+# Cleanup temporary credential
+Remove-Item -Force "C:\tempCred.xml"
