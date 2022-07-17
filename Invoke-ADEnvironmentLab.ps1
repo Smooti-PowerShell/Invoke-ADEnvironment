e1dfd4adebfc53ca45c69300d0840de80dca2d7f
smooti <kobeowens43099@gmail.com>
2022-07-17 15:59:00 -0400

    adding AD credentials

--- Invoke-ADEnvironmentLab.ps1
+++ Invoke-ADEnvironmentLab.ps1
@@ -3,18 +3,34 @@ param (
 	[SecureString]$DSRMPassword,
 
 	[Parameter(Mandatory = $False)]
+	[string]$DomainName = "testbed.local",
+
+	[Parameter(Mandatory = $False)]
 	[switch]$CreateTestUsers
 )
 
 Import-Module -Name .\modules\master.psm1
 
-# # Confirm we have an elevated session.
-# If (-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-# 	Throw "You must run this from an elevated PowerShell session."
-# }
+# Confirm we have an elevated session.
+If (-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	Throw ("You must run this from an elevated PowerShell session.")
+}
 
-# Schedule task
+# Create test user accounts in active directory
 if ($CreateTestUsers) {
+	# Verify credentials locally
+	$creds = Get-Credential -UserName $env:UserName -Message "Local Account Password"
+	$password = $creds.GetNetworkCredential().Password
+	Add-Type -AssemblyName System.DirectoryServices.AccountManagement 
+	$DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
+	$credentialsValid = $DS.ValidateCredentials("smooti", $creds.GetNetworkCredential().Password)
+	if ($credentialsValid -ne "True") {
+		throw ("The local credentials that you have provided are invalid.")
+	}
+
+	# Create active directory credentials
+	$adCreds = New-Object System.Management.Automation.PSCredential ("$($DomainName)\$env:UserName", $password)
+	$adCreds | Export-Clixml -Path "C:\tempCred.xml"
 	$scripty = "$($env:PSScriptRoot)\New-ADTestUsers.ps1"
 	$params = @{
 		TaskName     = "Test"
@@ -22,6 +38,7 @@ if ($CreateTestUsers) {
 		TaskArgument = "`"-NonInteractive -WindowStyle Normal -NoLogo -NoProfile -NoExit -Command `“&`”$($scripty)`”"
 	}
 	
+	# Schedule task
 	Invoke-Task @params
 }
 
@@ -36,7 +53,7 @@ $DSRMPassword = ConvertTo-SecureString $DSRMUnencryptedPassword -AsPlainText -Fo
 
 # Setup AD forrest
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Build-ADForrest -DSRMPassword $DSRMPassword -DomainName "testbed.local"
+Build-ADForrest -DSRMPassword $DSRMPassword -DomainName $DomainName
 
 # Apply changes
 Restart-Computer
--- New-ADTestUsers.ps1
+++ New-ADTestUsers.ps1
@@ -1,5 +1,6 @@
 # Create test users
+$adCreds = Import-Clixml "C:\tempCred.xml"
 $testUsers = @("Robb Stark", "Theon Greyjoy", "Ned Stark", "Aria Stark")
 foreach ($user in $testUsers) {
-	New-ADUser -Name $user -Enabled $False
+	New-ADUser -Name $user -Enabled $False -Credential $adCreds
 }
