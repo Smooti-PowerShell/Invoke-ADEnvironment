85bc3dec3053e3743796c0e66380e12fa9987fee
smooti <kobeowens43099@gmail.com>
2022-07-17 16:42:17 -0400

    adding json config and cleanup

--- New-ADTestUsers.ps1
+++ New-ADTestUsers.ps1
@@ -1,3 +1,6 @@
+#Getting information from the json file
+$config = Get-Content "$($env:PSScriptRoot)\config.json" | ConvertFrom-Json
+
 # Create test users
 $adCreds = Import-Clixml "C:\tempCred.xml"
 $testUsers = @("Robb Stark", "Theon Greyjoy", "Ned Stark", "Aria Stark")
@@ -5,5 +8,6 @@ foreach ($user in $testUsers) {
 	New-ADUser -Name $user -Enabled $False -Credential $adCreds
 }
 
-# Cleanup temporary credential
+# Cleanup
 Remove-Item -Force "C:\tempCred.xml"
+Unregister-ScheduledTask -TaskName $config.InvokeTask.TaskName -Confirm:$false
