34bc0abe0a6d076028e3e96c0cf298c8553b0d18
smooti <kobeowens43099@gmail.com>
2022-11-12 14:08:29 -0500

    Removing tests

--- tests/New-ADTestUsers.ps1
+++
@@ -1,6 +0,0 @@
-# Create test users
-$adCreds = Import-Clixml "C:\tempCred.xml"
-$testUsers = @("Robb Stark", "Theon Greyjoy", "Ned Stark", "Aria Stark")
-foreach ($user in $testUsers) {
-	New-ADUser -Name $user -Enabled $False -Credential $adCreds
-}
